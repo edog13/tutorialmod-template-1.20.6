b1e6089e662e05900707f448189a724b2d781e40
Ethan Dior <ejdior13@gmail.com>
2024-04-30 15:12:49 -0400

    Initial commit

---
+++ src/main/java/net/ethan/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.ethan.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
